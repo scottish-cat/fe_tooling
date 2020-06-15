f0e1bcd1e9a0539e2e07732c4d230332e96ae73f
Maryna Butovych <marina.butovich@gmail.com>
2020-06-15 03:19:08 +0300

    output config (Generating HTML using plugin, clearing dist folder)

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new CleanWebpackPlugin(),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
