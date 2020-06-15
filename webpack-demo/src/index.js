2e32d71cda5459ee43738b0c845b21bbf1ff96dd
Maryna Butovych <marina.butovich@gmail.com>
2020-06-15 03:09:26 +0300

    asset setup (css, image, svg)

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,21 @@
 import join from 'lodash/join';
+import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = join(['Hello', 'webpack'], ' ');
+  const element = document.createElement('div');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const br = document.createElement('br');
+  element.appendChild(br);
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.width = 500;
+  element.appendChild(myIcon);
+
+  return element;
+}
   
-    return element;
-  }
-  
-  document.body.appendChild(component());
+document.body.appendChild(component());
